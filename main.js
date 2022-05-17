df3e8a1e9f7c644be91ae51b4539c82d498208d9
Cheol-Cheol <dream174822@gmail.com>
2022-05-17 14:28:33 +0900

    ✨ inputNum 기능 구현

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-function Calculator() {
-  console.log("hi");
-}
+let left = null,
+  oper = null,
+  right = null;
 
-const cal = new Calculator();
+const inputNum = (num) => {
+  const inpEl = document.querySelector(".inp");
+  if (oper === null) {
+    if (left === null) {
+      left = `${num}`;
+    } else left += num;
+    inpEl.value = left;
+  } else {
+    if (right === null) {
+      right = `${num}`;
+    } else right += num;
+    inpEl.value = right;
+  }
+  console.log("left:", left);
+  console.log("right:", right);
+};
