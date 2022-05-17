b21fadf33c9fb9d0559d6f5712cd54a94b078083
Cheol-Cheol <dream174822@gmail.com>
2022-05-17 15:33:32 +0900

    ✨ render 기능 구현

--- main.js
+++ main.js
@@ -2,27 +2,39 @@ let left = null,
   oper = null,
   right = null;
 
+// 숫자 입력
 const inputNum = (num) => {
   const clearEl = document.querySelector(".clear");
   clearEl.textContent = "C";
-  const inpEl = document.querySelector(".inp");
+
   if (oper === null) {
     if (left === null) {
       left = `${num}`;
     } else left += num;
-    inpEl.value = left;
   } else {
     if (right === null) {
       right = `${num}`;
     } else right += num;
-    inpEl.value = right;
   }
   console.log("left:", left);
   console.log("right:", right);
+  render();
 };
 
-const inputOper = (op) => {};
+const render = () => {
+  const inpEl = document.querySelector(".inp");
+  if (oper === null) {
+    inpEl.value = left;
+  } else {
+    inpEl.value = right;
+  }
+};
 
+const inputOper = (op) => {
+  oper = op;
+};
+
+// 초기화 버튼
 const clearBtn = () => {
   const clearEl = document.querySelector(".clear");
   const inpEl = document.querySelector(".inp");
